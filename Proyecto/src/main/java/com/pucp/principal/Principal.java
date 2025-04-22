ef0c8ab1870af62fb2f5628d0ce712c6824624d9
OmarJimenez41 <a20216287@pucp.edu.pe>
2025-04-22 00:50:52 -0500

    Principal.java

--- Proyecto/src/main/java/com/pucp/principal/Principal.java
+++ Proyecto/src/main/java/com/pucp/principal/Principal.java
@@ -3,7 +3,8 @@
  */
 
 package com.pucp.principal;
-
+import com.pucp.modelo.usuarios.Usuario;
+import com.pucp.modelo.usuarios.Administrator;
 /**
  *
  * @author Axel
@@ -11,6 +12,12 @@ package com.pucp.principal;
 public class Principal {
 
     public static void main(String[] args) {
+        Usuario usuario1= new Usuario();
+        Administrator admin1= new Admin();
+
+        UsuarioCRUD usuarioCRUD = new UsuarioCRUD();
+        AdministratorCRUD adminCRUD = new AdministratorCRUD();
+        
         System.out.println("Hola davmos!");
     }
 }
